8f0728618928d086edaf107ad86a85c97716461a
dartbill <billieldartnell@hotmail.com>
2022-05-11 10:22:20 +0100

    when search button clicked cake array is printed to console

--- js/index.js
+++ js/index.js
@@ -1,76 +1,96 @@
-// ********************************************
-// SETUP
-const btn = document.querySelector('button');
-const form = document.querySelector('form');
+// // ********************************************
+// // SETUP
+// const btn = document.querySelector('button');
+// const form = document.querySelector('form');
 
-// Bind event listeners
-btn.addEventListener('click', getMessage);
-form.addEventListener('submit', submitPet);
+// // Bind event listeners
+// btn.addEventListener('click', getMessage);
+// form.addEventListener('submit', submitPet);
 
-// Fetch all pets as soon as app is loaded
-getAllPets();
+// // Fetch all pets as soon as app is loaded
+// getAllPets();
 
-// ********************************************
-// Pets Flow
-// index
-function getAllPets(){
-    fetch('http://localhost:3000/pets')
-        .then(r => r.json())
-        .then(appendPets)
-        .catch(console.warn)
-};
+// // ********************************************
+// // Pets Flow
+// // index
+// function getAllPets(){
+//     fetch('http://localhost:3000/cakes')
+//         .then(r => r.json())
+//         .then(appendPets)
+//         .catch(console.warn)
+// };
 
-// create
-function submitPet(e){
-    e.preventDefault();
-    console.log(e.target.petNameText.value);
-    const petData = {
-        name: e.target.petNameText.value,
-        age: e.target.petAge.value
-    };
+// // create
+// function submitPet(e){
+//     e.preventDefault();
+//     console.log(e.target.petNameText.value);
+//     const petData = {
+//         name: e.target.petNameText.value,
+//         age: e.target.petAge.value
+//     };
 
-    const options = { 
-        method: 'POST',
-        body: JSON.stringify(petData),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    };
+//     const options = { 
+//         method: 'POST',
+//         body: JSON.stringify(petData),
+//         headers: {
+//             "Content-Type": "application/json"
+//         }
+//     };
 
-    fetch('http://localhost:3000/pets', options)
-        .then(r => r.json())
-        .then(appendPet)
-        .catch(console.warn)
-};
+//     fetch('http://localhost:3000/pets', options)
+//         .then(r => r.json())
+//         .then(appendPet)
+//         .catch(console.warn)
+// };
 
-// helpers
-function appendPets(pets){
-    pets.forEach(appendPet);
-};
+// // helpers
+// function appendPets(pets){
+//     pets.forEach(appendPet);
+// };
 
-function appendPet(petData){
-    const newLi = document.createElement('li');
-    newLi.textContent = `Name: ${petData.name} || Age: ${petData.age}`
-    const petsList = document.querySelector('ul');
-    petsList.append(newLi);
-};
+// function appendPet(petData){
+//     const newLi = document.createElement('li');
+//     newLi.textContent = `Name: ${petData.name} || Age: ${petData.age}`
+//     const petsList = document.querySelector('ul');
+//     petsList.append(newLi);
+// };
 
-// ********************************************
+// // ********************************************
 
-// MESSAGE FLOW
-function getMessage(){
-    fetch('http://localhost:3000')
-        .then(r => r.text())
-        .then(renderMessage)
-        .catch(console.warn)
-};
+// // MESSAGE FLOW
+// function getMessage(){
+//     fetch('http://localhost:3000')
+//         .then(r => r.text())
+//         .then(renderMessage)
+//         .catch(console.warn)
+// };
 
-function renderMessage(msgText){
-    const msg = document.createElement('p');
-    msg.textContent = msgText;
-    msg.style.color = 'red';
-    document.body.append(msg);
-};
+// function renderMessage(msgText){
+//     const msg = document.createElement('p');
+//     msg.textContent = msgText;
+//     msg.style.color = 'red';
+//     document.body.append(msg);
+// };
 
-// ********************************************
+// // ********************************************
 
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (e)=> {
+getRandom(e)
+})
+
+function getRandom(e) {
+
+  e.preventDefault();
+
+  
+  fetch('http://localhost:3000/cakes')
+    .then(resp => resp.json())
+    .then((data)=>{
+        console.log(data)
+    //   document.getElementById('text').textContent = data
+    })
+
+    .catch(console.warn)
+}
